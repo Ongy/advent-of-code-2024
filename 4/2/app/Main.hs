47ddb5c30127f4ca6468f6ca6c69d89cb7db6e03
Markus Ongyerth <ongy@ongy.net>
2024-12-04 06:52:13 +0000

    Format in cross shape

--- 4/2/app/Main.hs
+++ 4/2/app/Main.hs
@@ -12,11 +12,19 @@ doThing input = let
 
           countXMASHorizontal :: String -> String -> String -> Word
           countXMASHorizontal xs@(_:xs') ys@(_:ys') zs@(_:zs') = countXMASHorizontal xs' ys' zs' + case (xs, ys, zs) of
-            ('M':_:'M':_, _:'A':_:_, 'S':_:'S':_) -> 1 
-            ('S':_:'M':_, _:'A':_:_, 'S':_:'M':_) -> 1 
-            ('M':_:'S':_, _:'A':_:_, 'M':_:'S':_) -> 1 
-            ('S':_:'S':_, _:'A':_:_, 'M':_:'M':_) -> 1 
-            (_          , _        , _          ) -> 0
+            ('M': _ :'M':_,
+              _ :'A': _ :_,
+             'S': _ :'S':_) -> 1 
+            ('S': _ :'M':_,
+              _ :'A': _ :_,
+             'S': _ :'M':_) -> 1 
+            ('M': _ :'S':_,
+              _ :'A': _ :_,
+             'M': _ :'S':_) -> 1 
+            ('S': _ :'S':_,
+              _ :'A': _ :_,
+             'M': _ :'M':_) -> 1 
+            _ -> 0
           countXMASHorizontal _ _ _ = 0
             
 
